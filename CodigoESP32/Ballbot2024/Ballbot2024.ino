426d94c7e21e987b054215a1866f79f8121204d5
Rafael Cuellar-Ramirez <rafael.cuellarr@ingenieria.unam.edu>
2024-11-21 13:08:31 -0600

    comment changes

--- CodigoESP32/Ballbot2024/Ballbot2024.ino
+++ CodigoESP32/Ballbot2024/Ballbot2024.ino
@@ -1,21 +1,37 @@
-//////////////LIBRERIAS////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//          Facultad de Ingenieria UNAM                    //
+//          Taller de Robótica Abierta                     //
+//                                                         //
+//    Proyect: Ballbot                                     //
+//                                                         //
+//    Description: Digital state feedback control to       //
+//    stabilize a ballbot.                                 //
+//                                                         //
+//    Franco Ángel Jasso Osorio                            //
+//    Rafael Cuéllar Ramírez                               //
+//                                                         //
+//    2024 MIT License                                     //
+/////////////////////////////////////////////////////////////
+
+// LIBRARIES
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
 #include <PID_v1.h> // bilioteca PID 
 #include <BluetoothSerial.h>
 
-//////////////////////Variables geométricas//////////////////////////
-//w: wheel    b:ball
-const float rw = 0.03;  //metros
-const float rb = 0.274;  //bola cargada
-//const float rb = 0.12;   //bola basquet
-float theta_w = 0;      //angulo de la rueda 1 medido por el encoder
+// Physical Parameters (w: wheel    b: ball    r: robot)
+const float rw = 0.03;  // [m]
+const float rb = 0.274;  // bola cargada
+//const float rb = 0.12;   // bola basquet
+float angle_w1 = 0, angle_w2 = 0, angle_w3 = 0; 
 float theta_b = 0, theta_b_x = 0, theta_b_y = 0;      //angulo de la pelota (variable de estado)
 float beta = 0;
-//////////////VARIABLES PARA LA IMU///////////////////////////////////////////////////////////////////////////////
+
+// IMU
 MPU6050 mpu;
 #define INTERRUPT_PIN  19  // use pin 2 on Arduino Uno & most boards
+float yaw=0,pitch=0,roll=0;
 
 bool dmpReady = false;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
@@ -24,7 +40,7 @@ uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-/*Orientation/motion vars*/
+// Motion
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
@@ -32,15 +48,6 @@ VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measure
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-/*INTERRUPT DETECTION ROUTINE*/
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() 
-{
-    mpuInterrupt = true;
-}
-/*Variables de la IMU*/
-float yaw=0,pitch=0,roll=0;
 
 //////////////VARIABLES PARA MOTORES/////////////////////////////////////////////////////////////////////////////////////
 int dir1=18, dir2=17, dir3=4;   
@@ -80,6 +87,14 @@ float kp=80, kd=0.8, ki=8.0;
 float T_x = 0, T_y = 0;
 /*Para el tiempo*/
 float tact=0, tant=0, tmuestreo=100;
+
+/*INTERRUPT DETECTION ROUTINE*/
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+}
+
 ///////////////////////////////////////////VOID SETUP////////////////////////////////////////////////////////////
 void setup() 
 {  
